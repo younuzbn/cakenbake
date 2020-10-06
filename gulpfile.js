49e86367d9023519395b1905e9f6f21b75b62885
younuz mohd <younuzbn@gmail.com>
2020-10-06 12:43:14 +0530

    l

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require( 'gulp' );
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+    return gulp.src('src/style/sass/*.sass')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(gulp.dest('./src/style/css'));
+});
+
+gulp.task( 'watch', function() {
+  gulp.watch( './src/style/**/*.sass', [ 'sass' ] );
+});
+
+gulp.task('default', ['watch' , 'sass']);
